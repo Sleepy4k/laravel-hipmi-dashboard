a85ed30803b6d40e6caefb3cdad21a4331ef150f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-10-27 20:40:21 +0700

    chore(config): update vite config to support SRI

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import manifestSRI from 'vite-plugin-manifest-sri';
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
       refresh: true,
     }),
     react(),
+    manifestSRI(),
   ],
 });
